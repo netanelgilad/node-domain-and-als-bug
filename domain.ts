7fa7c62cb015200e903a0e82bff8b3323daa7050
Netanel Gilad <netanelg@wix.com>
2024-06-20 14:34:04 +0300

    clearer

--- domain.ts
+++ domain.ts
@@ -1,13 +1,12 @@
 import domain from "domain";
 
-const aDomain = domain.create();
-
 console.log((process as any).domain?.dataFromClient);
 setInterval(() => {
   console.log((process as any).domain?.dataFromClient);
 }, 1000);
 
 setTimeout(async () => {
+  const aDomain = domain.create();
   aDomain["dataFromClient"] = "data";
   aDomain.enter();
   await new Promise((resolve) => setTimeout(resolve, 3000));
